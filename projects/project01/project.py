331702ddf94553c33374e08d0e8d55ef082175e8
j2doan <j2.doan@san.rr.com>
2025-04-15 09:47:37 -0700

    project1 and lec04 done

--- projects/project01/project.py
+++ projects/project01/project.py
@@ -27,7 +27,28 @@ warnings.filterwarnings("ignore")
 
 
 def create_detailed_schedule(schedule, stops, trips, bus_lines):
-    ...
+
+    schedule_stops = schedule.merge(stops, on='stop_id', how='left')
+    detailed_schedule = schedule_stops.merge(trips, on='trip_id', how='left')
+
+    detailed_schedule['route_id'] = pd.Categorical(detailed_schedule['route_id'], categories=bus_lines, ordered=True)
+
+    detailed_schedule = detailed_schedule.sort_values(by=['route_id', 'trip_id', 'stop_sequence']) # account for multiple trip_id in a route_d, and then order by stop_sequence 12345...
+    
+    # IDEA: instead of for loop, make a new column for trip_lengths (based on stops), and then assign those total trip_lengths to each trip_id, then can sort faster! 
+    trip_lengths = detailed_schedule.groupby('trip_id')['stop_id'].count() # make new col
+    detailed_schedule['trip_length'] = detailed_schedule['trip_id'].map(trip_lengths) # map them to the correct trip_ids
+    
+    detailed_schedule = detailed_schedule.sort_values(by=['route_id', 'trip_length', 'trip_id', 'stop_sequence'])  # fewer stops come FIRST
+    
+    detailed_schedule = detailed_schedule.drop(columns=['trip_length']) # drop the temporary trip_len col
+    
+    detailed_schedule = detailed_schedule.set_index('trip_id') # set index
+
+    detailed_schedule = detailed_schedule[detailed_schedule['route_id'].isna() == False]
+    
+    return detailed_schedule
+
 
 def visualize_bus_network(bus_df):
     # Load the shapefile for San Diego city boundary
@@ -63,7 +84,29 @@ def visualize_bus_network(bus_df):
         margin={"r":0,"t":0,"l":0,"b":0}
     )
 
-    ...
+    color_palette = px.colors.qualitative.Plotly # get the colors
+    route_ids = bus_df['route_id'].unique() # get all the unique route_ids
+    color_map = {route_id: color_palette[i % len(color_palette)] for i, route_id in enumerate(route_ids)} # create a dictionary for each route_id:color
+    # Example: if route 201 is first, it might get blue, route 202 might get orange, etc.
+
+    # Group by trip_id to plot each route
+    for trip_id, trip_data in bus_df.groupby(level=0): # not aggreggating data, need index (trip_id), and the rest of the data in that group
+        #trip_data_sorted = trip_data#.sort_values(by='stop_sequence')
+
+        fig.add_trace(go.Scattermapbox(
+            lat=trip_data['stop_lat'], # a series of stop_lat
+            lon=trip_data['stop_lon'], # series of stop_lon
+            mode='markers+lines',
+            marker=go.scattermapbox.Marker(size=8),
+            line=go.scattermapbox.Line(width=2, color=color_map[trip_data['route_id'].iloc[0]]), # make the color of the line match with the route in the dictionary
+            text=trip_data['stop_name'],
+            hoverinfo='text',
+            name=f"Route {trip_data['route_id'].iloc[0]} ({trip_id})" # EX: Route 201 (17287595)
+        ))
+
+    return fig
+
+
 
 
 # ---------------------------------------------------------------------
